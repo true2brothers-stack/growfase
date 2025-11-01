0a4474e53e0922fe08db2eacbbce7b211996fa61
TrueBrothers (via WeWeb) <true2brothers@gmail.com>
2025-11-01 13:47:25 +0000

    v64 - growfase.v3

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 63;
+const version = 64;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
